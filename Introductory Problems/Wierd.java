d49f12b2d27eea7c5b27bf792d576848638c361f
amitkr79 <amitraj8655977@gmail.com>
2026-01-13 23:36:11 +0530

    solved first problem

--- Introductory Problems/Wierd.java
+++ Introductory Problems/Wierd.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 class Wierd{
     public static void main(String args[]){
-        System.out.println("hello world");
+       
+        Scanner in = new Scanner(System.in);
+
+        long n = in.nextLong();
+        while(true){
+            System.out.print(n+" ");
+            if(n==1) break;
+            if(n%2==0){
+                n/=2;
+            }else{
+                n = n*3+1;
+            }
+        }
     }
 }
